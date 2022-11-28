1377be041ab88752ad37fabfb53880e3d05ded49
Yunos_T <yunoshevv2000@gmail.com>
2022-11-28 13:05:11 +0200

    added about page swiper

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,45 +1,15 @@
 import React from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay } from 'swiper';
 
-import swiperImage1 from '../../images/swiper-image-1.png';
-import swiperImage2 from '../../images/swiper-image-2.png';
-import swiperImage3 from '../../images/swiper-image-3.png';
-import swiperImage4 from '../../images/swiper-image-4.png';
+
+import AboutSwiper from './items/AboutSwiper';
 
 import './style.scss';
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
 
 const About = () => {
   return (
     <div className="about_wrapper">
       <div className="about_container">
-        <Swiper
-          className="about_swiper"
-          slidesPerView={1}
-          spaceBetween={30}
-          loop={true}
-          autoplay={{
-            delay: 2500,
-            disableOnInteraction: false,
-          }}
-          modules={[Autoplay]}
-        >
-          <SwiperSlide>
-            <img src={swiperImage1} alt="swiperImage1" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={swiperImage2} alt="swiperImage2" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={swiperImage3} alt="swiperImage3" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={swiperImage4} alt="swiperImage4" />
-          </SwiperSlide>
-        </Swiper>
+        <AboutSwiper />
       </div>
     </div>
   );
---
+++ src/pages/About/items/AboutSwiper/index.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Navigation, Pagination, Parallax } from 'swiper';
+
+import swiperImage1 from '../../../../images/image-on-swiper-1.png';
+import swiperImage2 from '../../../../images/image-on-swiper-2.png';
+import swiperImage3 from '../../../../images/image-on-swiper-3.png';
+import swiperImage4 from '../../../../images/image-on-swiper-4.png';
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+import './style.scss';
+
+const AboutSwiper = () => {
+  return (
+    <>
+      <Swiper
+        className="about_swiper"
+        slidesPerView={1}
+        spaceBetween={120}
+        speed={1500}
+        loop={true}
+        autoplay={{
+          delay: 3500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          type: 'progressbar',
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation, Autoplay]}
+      >
+        <SwiperSlide>
+          <div className="slide_title">
+            <h2>About Yopta Store</h2>
+            <p>This store was created for entertainment and educational purposes, as part of a self-education course in learning web programming.</p>
+            <p>Thanks to SnailDog and Muloversic!</p>
+          </div>
+          <img className="slide_image" src={swiperImage1} alt="swiperImage1" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className="slide_image" src={swiperImage2} alt="swiperImage2" />
+          <div className="slide_title">
+            <h2>About Yopta Store</h2>
+            <p>This store was created for entertainment and educational purposes, as part of a self-education course in learning web programming.</p>
+            <p>Thanks to SnailDog and Muloversic!</p>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="slide_title">
+            <h2>About Yopta Store</h2>
+            <p>This store was created for entertainment and educational purposes, as part of a self-education course in learning web programming.</p>
+            <p>Thanks to SnailDog and Muloversic!</p>
+          </div>
+          <img className="slide_image" src={swiperImage3} alt="swiperImage3" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className="slide_image" src={swiperImage4} alt="swiperImage4" />
+          <div className="slide_title">
+            <h2>About Yopta Store</h2>
+            <p>This store was created for entertainment and educational purposes, as part of a self-education course in learning web programming.</p>
+            <p>Thanks to SnailDog and Muloversic!</p>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </>
+  );
+};
+
+export default AboutSwiper;
