37a149a7cf4bc88cb020be3387f4ada73534d314
Yunos_T <yunoshevv2000@gmail.com>
2022-12-26 15:00:58 +0200

    added redux form

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,12 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import { usersReducer } from './reducers/usersReducer';
+import { reducer as formReducer } from 'redux-form'
+
 import logger from 'redux-logger';
 
 const reducer = combineReducers({
   users: usersReducer,
+  form: formReducer,
 });
 
-export const store = configureStore({ reducer, middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger) });
+export const store = configureStore({ reducer , middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger) });
